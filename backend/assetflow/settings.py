ea37329fb77df1aa5f366bc16f4e5ffc7bba32d6
febin thomas <tfebin39@gmail.com>
2026-07-12 13:53:37 +0530

    added env

--- backend/assetflow/settings.py
+++ backend/assetflow/settings.py
@@ -9,23 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from datetime import timedelta
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(0!7al8u+ih*3iv47hge6u80z(b43tq(sv5je3w8+g5j)2kl3j'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-if-missing')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
--- backend/assets/views/login.py
+++ backend/assets/views/login.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-import re
+# import re
 
 from assets.models import Admin, Department, Employee, AssetManager
 
